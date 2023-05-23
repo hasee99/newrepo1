9d2dcf35b6fffae73c5f258100e472268768150b
hasee99 <132909228+hasee99@users.noreply.github.com>
2023-05-23 08:56:56 +0530

    Update hasee99.sh

--- hasee99.sh
+++ hasee99.sh
@@ -2,3 +2,29 @@
 cd/home/opc/
 mkdir micro11
 /home/opc/micro11
+node{
+  def remote = [:]
+  remote.name = 'oraclevm'
+  remote.host = '152.67.160.182'
+  remote.user = 'opc'
+  remote.password = 'Muzammil073#'
+  remote.allowAnyHosts = true
+    stage('checkout') {
+           checkout scm
+  }  
+  stage('Remote SSH') {
+   // writeFile file: 'abc.sh', text: 'ls -lrt'
+   // sshScript remote: remote, script: "abc.sh"
+    sshCommand remote : remote, command: "pwd"
+      sshCommand remote : remote, command: "cd /home"
+    sshCommand remote : remote, command: "pwd"
+      sshCommand remote : remote, command: "ls -lrt"
+  }     
+  
+   stage('Remote SSH 2') {
+   sshScript remote: remote, script: "./micro/print.sh"
+  }  
+  stage('Remote SSH 3') {
+   sshScript remote: remote, script: "./micro/args.sh"
+  }  
+        }
